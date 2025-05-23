fad6baeb0267e2ba34b86838a05d1b50d1ad2ec6
basanth1 <pbasanthkumar60@gmail.com>
2025-05-23 16:25:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,8 @@ def checkout():
 def clear_cart():
     session.pop("cart", None)
     return redirect(url_for("orders"))
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory("static", filename)
 if __name__ == "__main__":
     app.run(debug=True)
